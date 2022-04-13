d5b16c5ce61bf811165034962754d31230d8e6b1
蛮吉 <71599447+manjixx@users.noreply.github.com>
2022-04-13 23:29:22 +0800

    init repo

---
+++ src/main/java/com/hoo/community/CommunityApplication.java
@@ -0,0 +1,17 @@
+package com.hoo.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        try{
+            SpringApplication.run(CommunityApplication.class, args);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
