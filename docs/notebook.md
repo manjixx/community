449a66fedcaa4125be9309faa5c511b5f0963882
manjixx <xiaochuiwei@gmail.com>
2022-04-17 00:08:26 +0800

    jicheng mybatis and insert data

--- docs/notebook.md
+++ docs/notebook.md
@@ -7,8 +7,10 @@
 + [github OAuth]()
 
 # 5.申请github APP 从github登录
+
 * 注册github APP 文档说明
     + github页面底层API —— Github Docs Developers - APPS -Building OAuth APPS
+
 * 注册github APP
     + Settings-develop Settings - OAuth Apps
 
@@ -40,3 +42,65 @@ springboot启动时，会以map的形式将文件中的值放入容器，通过`
   
 * Session 和cookie原理
   + 登录成功后显示登录态
+  + session 相当于银行账户
+    + 通过 ```HttpServletRequest```获取到session
+    + 当我们把````HttpServletRequest```写入到方法时，SpringBoot会自动将上下文中的request放到中间供我们使用
+    + 获取到session，之后需要在index.html中去判断是否拿到session-百度 如何使用thymeleaf 取session 
+  + cookie 相当于银行银行卡
+  
++ 图解MySQL并学习其基本用法
+  + UML详解-麻匠社区微信公众号
+    + 泛化关系
+    + 实现关系
+    + 聚合关系
+    + 组合关系
+    + 关联关系
+    + 依赖关系
+    + 实箭泛化虚实现，虚线依赖实关联，空菱聚合实组合，项目沟通图常见
+  + MySQL
+    + 自然型解释语言
+    + database table record
+    + CRUD
+  + H2
+    + 特点：
+      + 非常快，开源的JDBC API
+      + 可直接内置到server里边去，内存数据库
+      + 可通过浏览器直接去访问
+      + small footprin：about 2MB jar file，为什么选它的原因，通过jar依赖可以直接放入文件
+      + 无法创建表：[https://blog.csdn.net/nruuu/article/details/123909184]
+      + 
+        ```SQL
+            CREATE TABLE `user`(
+            `id` INT AUTO_INCREMENT PRIMARY KEY,
+            `account_id` VARCHAR(100),
+            `name` VARCHAR(50),
+            `token` CHAR(36),
+            `gmt_create` BIGINT,
+            `gmt_modified` BIGINT);
+        ```
+        
++ P16 集成 Mybaits 并实现插入数据
+  + http://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/
+  + Spring 官方文档[https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support]
+  + 定义数据库连接池：
+  ```
+    spring.datasource.url=jdbc:mysql://localhost/test
+    spring.datasource.username=dbuser
+    spring.datasource.password=dbpass
+    spring.datasource.driver-class-name=com.mysql.jdbc.Driver
+  ```
+  + Mybatis
+    + Mybatis会自动解析state中的内容填入#{state}中
+    ```java
+        @Mapper
+        public interface UserMapper {
+        @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{account_id},#{token},#{gmt_create},#{gmt_modified})")
+        void insert(User user);
+        }
+    ```
+  + 网络与网络之间传输数据用dto，数据传递数据用model
+  + SpringBoot启动项目时：Cannot load driver class: org.h2.Driver,缺乏依赖，添加Maven: com.h2database:h2:1.4.200
+  
++ P17 实现持久化登录状态获取
+  + P16
+  + 手动模拟cookie和session的交互方式，以实现在服务器宕机或重启时，用户都可以重新登录，并保持登录态
--- src/main/java/com/hoo/community/controller/AuthorizeController.java
+++ src/main/java/com/hoo/community/controller/AuthorizeController.java
@@ -2,12 +2,18 @@ package com.hoo.community.controller;
 
 import com.hoo.community.dto.AccessTokenDTO;
 import com.hoo.community.dto.GithubUser;
+import com.hoo.community.mapper.UserMapper;
+import com.hoo.community.model.User;
 import com.hoo.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import sun.misc.UUDecoder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -20,9 +26,12 @@ public class AuthorizeController {
     private String clientSecret;
     @Value("${github.redirect.uri}")
     private String  redirectUri;
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code){
+    public String callback(@RequestParam(name = "code") String code,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -30,10 +39,21 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         // 利用获取得到的code值，获取token值
         String  accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        System.out.println(accessToken);
         // 利用得到的token获取用户信息
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            // 登录成功。写cookie 和 session
+            request.getSession().setAttribute("githubUser",githubUser);
+        }else{
+            // 登录失败，重新登录
+        }
+        return "redirect:/";    // 重定向，跳转回主页
     }
 }
